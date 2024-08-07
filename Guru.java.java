1f04349a34fa394e9c1e02df4c70688835a79a19
LaksitaHerlyana <laksita_purnomo_33rpl@student.smktelkom-mlg.sch.id>
2024-08-07 14:55:30 +0700

    Add files via upload

---
+++ Guru.java.java
@@ -0,0 +1,19 @@
+//object Class
+public class Guru{
+    String guru;
+    int id;
+    String nama;
+    String mapel;
+    String alamat;
+
+    //Constructor
+    public Guru() {
+        id = 1;
+        nama = "Aulia, Diaur, Pasha, Andre,Feni";
+        mapel = "Produktif RPL, Ipas, Produktif RPL, Bahasa Inggris,Matematika ";
+        alamat = "Malang, Malang, Malang, Malang, Malang";
+
+
+    
+}
+}
